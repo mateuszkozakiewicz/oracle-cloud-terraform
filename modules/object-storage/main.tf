4cb5be72d564da6b8d52b6c99b9637a2bbabb3d0
Mateusz Kozakiewicz <mateusz.kozakiewicz@boldare.com>
2025-08-01 14:48:38 +0200

    add replication policy

--- modules/object-storage/main.tf
+++ modules/object-storage/main.tf
@@ -9,7 +9,7 @@ resource "oci_objectstorage_bucket" "bucket" {
   access_type           = each.value.is_public == true ? "ObjectReadWithoutList" : "NoPublicAccess"
   storage_tier          = each.value.storage_tier
   object_events_enabled = lookup(each.value.optionals, "object_events_enabled", false)
-  versioning            = lookup(each.value.optionals, "versioning_enabled", false) ? "Enabled" : "Disabled"
+  versioning            = lookup(each.value.optionals, "versioning_enabled", "Disabled") ? "Enabled" : "Disabled"
 }
 
 resource "oci_objectstorage_object_lifecycle_policy" "lifecycle_policy" {
@@ -48,11 +48,28 @@ resource "oci_objectstorage_object_lifecycle_policy" "lifecycle_policy" {
 }
 resource "oci_objectstorage_replication_policy" "bucket_replication" {
   for_each = {
-    for k, v in var.buckets : k => v.optionals.replication_policy if v.optionals.replication_policy != null
+    for k, v in var.buckets : k => v if coalesce(v.optionals.replication_policy.source_region_name, "") == var.region
   }
   name                    = each.value.name
   bucket                  = oci_objectstorage_bucket.bucket[each.key].name
   namespace               = data.oci_objectstorage_namespace.namespace.namespace
-  destination_bucket_name = lookup(each.optionals.value, "destination_bucket_name", oci_objectstorage_bucket.bucket[each.key].name)
-  destination_region_name = each.value.optionals.destination_region
+  destination_bucket_name = coalesce(each.value.optionals.replication_policy.destination_bucket_name, oci_objectstorage_bucket.bucket[each.key].name)
+  destination_region_name = each.value.optionals.replication_policy.destination_region_name
+}
+
+data "oci_identity_compartment" "compartment" {
+  for_each = {
+    for k, v in var.buckets : k => v if coalesce(v.optionals.replication_policy.source_region_name, "") == var.region
+  }
+  id = each.value.compartment_id
+}
+
+resource "oci_identity_policy" "replication_policy" {
+  for_each = {
+    for k, v in var.buckets : k => v if coalesce(v.optionals.replication_policy.source_region_name, "") == var.region
+  }
+  compartment_id = each.value.compartment_id
+  description    = "Policy for bucket replication from ${each.value.optionals.replication_policy.source_region_name} to ${each.value.optionals.replication_policy.destination_region_name}"
+  name           = "${each.value.name}-bucket-replication-policy-${each.value.optionals.replication_policy.source_region_name}-${each.value.optionals.replication_policy.destination_region_name}"
+  statements     = ["Allow service objectstorage-${each.value.optionals.replication_policy.source_region_name} to manage object-family in compartment ${data.oci_identity_compartment.compartment[each.key].name} where target.bucket.name = '${oci_objectstorage_bucket.bucket[each.key].name}'"]
 }
